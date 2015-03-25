edf4fd83b22cd642f80cfd39d18959f9c0a62d15
Yesudeep Mangalapilly <yesudeep@google.com>
2015-03-25 12:53:13 +0000

    Attempting to render an icon. Unsuccessful so far.

--- apps/angular_play/js/app.js
+++ apps/angular_play/js/app.js
@@ -1,4 +1,4 @@
-angular.module('MeowApp', []).controller('AppCtrl', function($scope) {
+angular.module('MeowApp', ['ngMaterial']).controller('AppCtrl', function($scope) {
   $scope.title1 = 'Meow 1';
   $scope.title4 = 'Meow 4';
 });

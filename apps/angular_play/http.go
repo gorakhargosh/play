da579a6cb42db1b9bc01a711379fdf830aaad76d
Yesudeep Mangalapilly <yesudeep@google.com>
2015-03-25 19:10:30 +0000

    What the fuck.

--- apps/angular_play/http.go
+++ apps/angular_play/http.go
@@ -1,26 +1,44 @@
 package angular_play
 
 import (
-	"fmt"
 	"html/template"
-	"log"
 	"net/http"
 )
 
-var indexTemplate = parseTemplate("indexTemplate", "templates/index.html")
+type ApplicationConfig struct {
+	Title string
+}
+
+var indexTemplate = parseTemplate(
+	"foobar",
+	"templates/base.html",
+	"templates/index.html")
+
+var testTemplate = parseTemplateString("foobar", `Example content.`)
 
 // parseTemplate parses a bunch of templates and assigns a nickname to them.
 func parseTemplate(nickname string, filenames ...string) *template.Template {
-	t, err := template.New(nickname).Delims("<<", ">>").ParseFiles(filenames...)
-	if err != nil {
-		log.Fatal(err)
-	}
+	t := template.New(nickname).Delims("<<", ">>")
+	// return template.Must(template.ParseFiles(filenames...))
+	// t, _ = t.ParseFiles(filenames...)
+	t = template.Must(t.ParseFiles("templates/base.html", "templates/index.html"))
 	return t
+}
+
+func parseTemplateString(nickname, content string) *template.Template {
+	t := template.New(nickname).Delims("<<", ">>")
+	return template.Must(t.Parse(content))
 }
 
 // indexHandler handles the root request URL path.
 func indexHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello")
+	applicationConfig := ApplicationConfig{
+		Title: "MeowApp",
+	}
+
+	if err := indexTemplate.ExecuteTemplate(w, "index.html", applicationConfig); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
 }
 
 // init initializes the module.

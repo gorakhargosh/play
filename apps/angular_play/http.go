f46685a87f5b813d2bd0e24d8f263e4e0af50ad6
Yesudeep Mangalapilly <yesudeep@google.com>
2015-03-25 14:17:49 +0000

    Run the angular play app with goapp.

--- apps/angular_play/http.go
+++ apps/angular_play/http.go
@@ -2,13 +2,28 @@ package angular_play
 
 import (
 	"fmt"
+	"html/template"
+	"log"
 	"net/http"
 )
 
+var indexTemplate = parseTemplate("indexTemplate", "templates/index.html")
+
+// parseTemplate parses a bunch of templates and assigns a nickname to them.
+func parseTemplate(nickname string, filenames ...string) *template.Template {
+	t, err := template.New(nickname).Delims("<<", ">>").ParseFiles(filenames...)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return t
+}
+
+// indexHandler handles the root request URL path.
 func indexHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello")
 }
 
+// init initializes the module.
 func init() {
-	http.HandleFunc('/', indexHandler)
+	http.HandleFunc("/", indexHandler)
 }
--- apps/angular_play/scripts/app.sh
+++ apps/angular_play/scripts/app.sh
@@ -2,4 +2,5 @@
 
 TOP_DIR=$(git rev-parse --show-toplevel)
 
-twistd -n web -p 8080 --path ${TOP_DIR}/apps/angular_play/
+#twistd -n web -p 8080 --path ${TOP_DIR}/apps/angular_play/
+${TOP_DIR}/bin/goapp serve --host=0.0.0.0 --port=5000 app.yaml

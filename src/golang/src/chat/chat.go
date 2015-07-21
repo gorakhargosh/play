383bac217304033773655395fe34945ba647b317
Yesudeep Mangalapilly <yesudeep@google.com>
2015-07-22 00:15:05 +0530

    Template loading chat.

---
+++ src/golang/src/chat/chat.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"html/template"
+	"log"
+	"net/http"
+	"path/filepath"
+	"sync"
+)
+
+type templateHandler struct {
+	// Enabling debug mode causes the template to be recompiled
+	// per request (useful during development).
+	debug bool
+	once  sync.Once
+
+	filename string
+	templ    *template.Template
+}
+
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	t.once.Do(func() {
+		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
+	})
+	t.templ.Execute(w, nil)
+}
+
+func main() {
+	http.Handle("/", &templateHandler{filename: "chat.html"})
+
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal("ListenAndServe:", err)
+	}
+}

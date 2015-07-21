c87c0e5a115d9df0823984a993b9ff522fb38403
Yesudeep Mangalapilly <yesudeep@google.com>
2015-07-22 00:28:24 +0530

    Reload templates when they change when using debug mode.

--- src/golang/src/chat/chat.go
+++ src/golang/src/chat/chat.go
@@ -1,12 +1,24 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"html/template"
 	"log"
 	"net/http"
 	"path/filepath"
 	"sync"
 )
+
+// Turns on debugging for the application; especially useful during
+// development:
+//
+// 1. Templates are compiled per request instead of per process instance
+//    allowing reloading the templates as you make changes.
+var debug = flag.Bool("debug", false, "turns on debugging")
+
+// The port to which the HTTP server will bind.
+var listenPort = flag.Int("port", 80, "listens on port number")
 
 type templateHandler struct {
 	// Enabling debug mode causes the template to be recompiled
@@ -18,17 +30,27 @@ type templateHandler struct {
 	templ    *template.Template
 }
 
+func compileTemplate(filename string) *template.Template {
+	return template.Must(template.ParseFiles(filepath.Join("templates", filename)))
+}
+
 func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	t.once.Do(func() {
-		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
-	})
+	if t.debug {
+		t.templ = compileTemplate(t.filename)
+	} else {
+		t.once.Do(func() {
+			t.templ = compileTemplate(t.filename)
+		})
+	}
 	t.templ.Execute(w, nil)
 }
 
 func main() {
-	http.Handle("/", &templateHandler{filename: "chat.html"})
+	flag.Parse()
 
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+	http.Handle("/", &templateHandler{filename: "chat.html", debug: *debug})
+
+	if err := http.ListenAndServe(fmt.Sprintf(":%d", *listenPort), nil); err != nil {
 		log.Fatal("ListenAndServe:", err)
 	}
 }

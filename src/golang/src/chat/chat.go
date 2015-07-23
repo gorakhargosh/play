67b6029813c2dbdbef83888614ef0b5761e3c300
Yesudeep Mangalapilly <yesudeep@google.com>
2015-07-23 23:18:25 +0530

    Chat stuff.

--- src/golang/src/chat/chat.go
+++ src/golang/src/chat/chat.go
@@ -44,7 +44,7 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 			t.templ = compileHTMLTemplate(t.filename)
 		})
 	}
-	t.templ.Execute(w, nil)
+	t.templ.Execute(w, r)
 }
 
 // Entry-point function.

e2e7001470a1de2a792c02eea61656f88cb2698e
Yesudeep Mangalapilly <yesudeep@google.com>
2015-07-27 00:28:00 +0530

    Quit channels.

---
+++ src/golang/src/concurrency/12-boring-generator-quit-channels.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+// Multiplexes reading from multiple channels fanning into a single channel
+// for the reader.
+func fanIn(a, b <-chan string) <-chan string {
+	c := make(chan string)
+	go func() {
+		for {
+			select {
+			case c <- <-a:
+			case c <- <-b:
+			}
+		}
+	}()
+	return c
+}
+
+// Generates endless messages on a channel that it returns.
+func boring(msg string, quit chan bool) chan string {
+	c := make(chan string)
+	go func() {
+		for i := 0; ; i++ {
+			select {
+			case c <- fmt.Sprintf("%s %d", msg, i):
+				time.Sleep(time.Duration(rand.Intn(1e3)) * time.Millisecond)
+			case <-quit:
+				return
+			}
+		}
+	}()
+	return c
+}
+
+func main() {
+
+	// Now we'll tell joe to shut up using the quit channel.
+	quit := make(chan bool)
+	joe := boring("Joe!", quit)
+
+	// Seed the random number generator first.
+	rand.Seed(time.Now().UTC().UnixNano())
+
+	for i := rand.Intn(10); i >= 0; i-- {
+		fmt.Println(<-joe)
+	}
+	quit <- true
+}

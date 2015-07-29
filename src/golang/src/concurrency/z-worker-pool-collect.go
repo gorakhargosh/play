e46f4fbf85ae0935692f7ac34e8dca58d4f8890d
Yesudeep Mangalapilly <yesudeep@google.com>
2015-07-29 11:13:23 +0530

    Holy fuck. That was amazing.

---
+++ src/golang/src/concurrency/z-worker-pool-collect.go
@@ -0,0 +1,94 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"os"
+	"strings"
+	"sync"
+)
+
+type Task interface {
+	Process()
+	Print()
+}
+
+type TaskGenerator func(line string) Task
+
+func run(r io.Reader, f TaskGenerator, workerCount int) {
+	var wg sync.WaitGroup
+
+	in := make(chan Task)
+	wg.Add(1)
+	go func() {
+		s := bufio.NewScanner(r)
+		for s.Scan() {
+			if len(strings.TrimSpace(s.Text())) > 0 {
+				in <- f(s.Text())
+			}
+		}
+		if s.Err() != nil {
+			log.Fatalf("Error reading input: %s", s.Err())
+		}
+		close(in)
+		wg.Done()
+	}()
+
+	// Start writing the work output to the output channel.
+	out := make(chan Task)
+	wg.Add(workerCount)
+	for i := 0; i < workerCount; i++ {
+		go func() {
+			for t := range in {
+				t.Process()
+				out <- t
+			}
+			wg.Done()
+		}()
+	}
+
+	// Close the channel when all the workers have done their work.
+	go func() {
+		wg.Wait()
+		close(out)
+	}()
+
+	for t := range out {
+		t.Print()
+	}
+}
+
+type lookupTask struct {
+	query string
+	ns    []*net.NS
+	err   error
+}
+
+func (t *lookupTask) Process() {
+	log.Println(*t)
+	ns, err := net.LookupNS(t.query)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	t.ns = ns
+}
+
+func (t *lookupTask) Print() {
+	fmt.Println("DNS servers: ")
+	for _, ns := range t.ns {
+		fmt.Printf("%s\n", ns.Host)
+	}
+	fmt.Println()
+}
+
+func NewLookupTask(line string) Task {
+	return &lookupTask{query: line}
+}
+
+func main() {
+	run(os.Stdin, NewLookupTask, 1000)
+}

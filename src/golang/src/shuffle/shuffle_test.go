c89ce3c259e4462dd1885ad16c91a3dba99f84bb
Yesudeep Mangalapilly <yesudeep@google.com>
2015-07-30 12:45:52 +0530

    Completely test shuffle.

--- src/golang/src/shuffle/shuffle_test.go
+++ src/golang/src/shuffle/shuffle_test.go
@@ -1,7 +1,6 @@
 package shuffle
 
 import (
-	"fmt"
 	"reflect"
 	"strings"
 	"testing"
@@ -40,11 +39,19 @@ func TestStringSliceShuffle(t *testing.T) {
 
 	ss2 := StringSlice(s2)
 
-	fmt.Println(ss1)
-	fmt.Println(ss2)
+	// fmt.Println(ss1)
+	// fmt.Println(ss2)
 
 	if reflect.DeepEqual(ss1, ss2) {
-		t.Fatal("Shuffle on identical slices failed")
+		t.Fatal("Shuffle on identical string slices failed")
+	}
+
+	s3 := make([]string, len(s1))
+	copy(s3, s1)
+	Strings(s3)
+	// fmt.Println(s3)
+	if reflect.DeepEqual(s1, s3) {
+		t.Fatal("Strings failed")
 	}
 }
 
@@ -58,10 +65,44 @@ func TestIntSliceShuffle(t *testing.T) {
 
 	ss2 := IntSlice(s2)
 
-	fmt.Println(ss1)
-	fmt.Println(ss2)
+	// fmt.Println(ss1)
+	// fmt.Println(ss2)
 
 	if reflect.DeepEqual(ss1, ss2) {
-		t.Fatal("Shuffle on identical slices failed")
+		t.Fatal("Shuffle on identical int slices failed")
+	}
+
+	s3 := make([]int, len(s1))
+	copy(s3, s1)
+	Ints(s3)
+	// fmt.Println(s3)
+	if reflect.DeepEqual(s1, s3) {
+		t.Fatal("Ints failed")
+	}
+}
+
+func TestFloat64SliceShuffle(t *testing.T) {
+	s1 := []float64{0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8, 9, 10}
+	s2 := make([]float64, len(s1))
+	copy(s2, s1)
+
+	ss1 := Float64Slice(s1)
+	ss1.Shuffle()
+
+	ss2 := Float64Slice(s2)
+
+	// fmt.Println(ss1)
+	// fmt.Println(ss2)
+
+	if reflect.DeepEqual(ss1, ss2) {
+		t.Fatal("Shuffle on identical float64 slices failed")
+	}
+
+	s3 := make([]float64, len(s1))
+	copy(s3, s1)
+	Float64s(s3)
+	// fmt.Println(s3)
+	if reflect.DeepEqual(s1, s3) {
+		t.Fatal("Float64s failed")
 	}
 }

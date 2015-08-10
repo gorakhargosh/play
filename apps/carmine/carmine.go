29e72c54e105d0d10d723ce80a24b2385b92753a
Yesudeep Mangalapilly <yesudeep@google.com>
2015-08-10 21:27:28 +0530

    Flags.

--- apps/carmine/carmine.go
+++ apps/carmine/carmine.go
@@ -5,25 +5,13 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 	"log"
 	"net/http"
 )
 
+// Holds the configuration for the application.
 var config Config
-
-func init() {
-	config = Config{}
-	flag.StringVar(&config.Host, "host", "", "the host on which to listen")
-	flag.IntVar(&config.Port, "port", 8080, "the port on which to listen")
-	flag.BoolVar(&config.Debug, "debug", false, "turns on debugging")
-	flag.StringVar(
-		&config.TemplatesDir,
-		"templatesDir",
-		"templates", "the directory that contains the templates")
-	flag.Parse()
-}
 
 func main() {
 	hostAddr := fmt.Sprintf("%s:%d", config.Host, config.Port)

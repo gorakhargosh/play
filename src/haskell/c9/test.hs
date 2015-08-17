e9a78db4a2f13e0041aa6e5b2ed541e9eb708a2c
Yesudeep Mangalapilly <yesudeep@google.com>
2015-08-17 12:26:02 +0530

    factorial.

--- src/haskell/c9/test.hs
+++ src/haskell/c9/test.hs
@@ -1,18 +1,24 @@
 double x = x + x
 
 quadruple = double . double
+-- quadruple x = double (double x)
+-- quadruple x = x + x + x + x
+-- quadruple x = x * 4
 
 n = a `div` length xs
-    where 
+    where
       a  = 10
       xs = [1..5]
 
 newlast xs = drop ((length xs) - 1) xs !! 0
-
 anotherlast xs = xs !! ((length xs) - 1)
 
 
 qs [] = []
-qs (x:xs) = qs [a | a <- xs, a < x] 
+qs (x:xs) = qs [a | a <- xs, a < x]
             ++ [x] ++
             qs [a | a <- xs, a >= x]
+
+
+factorial n = product [1..n]
+-- factorial n = product (take n [1..])

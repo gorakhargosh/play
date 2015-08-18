587dcdcfe171d98138066710bc335ab65f5a2021
Yesudeep Mangalapilly <yesudeep@google.com>
2015-08-18 21:07:42 +0530

    carmine/templates: adds documentation to the utility routines.

--- src/golang/src/carmine/default/src/templates.go
+++ src/golang/src/carmine/default/src/templates.go
@@ -1,21 +1,25 @@
 package main
 
-// parse parses templates given the templatePath and some filenames.
 import (
 	"html/template"
 	"path/filepath"
 )
 
+// We use these delimiters because client libraries like AngularJS use the
+// default template delimiters too.  As AngularJS allows the development of
+// reusable components, it is incredibly hard to change the template delimiters
+// for client-side templates.
 const (
 	leftDelim  = "{{{"
 	rightDelim = "}}}"
 )
 
+// parse parses a sequence of related templates while reading them from
+// the specified template path.
 func parse(templatePath string, filenames ...string) *template.Template {
 	paths := []string{}
 	for _, filename := range filenames {
 		paths = append(paths, filepath.Join(templatePath, filename))
 	}
-	return template.Must(
-		template.ParseFiles(paths...)).Delims(leftDelim, rightDelim)
+	return template.Must(template.ParseFiles(paths...)).Delims(leftDelim, rightDelim)
 }

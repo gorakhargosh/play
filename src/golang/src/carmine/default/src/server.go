a5cbe66dd1fbfb24164bc1c4ccfb744eb9eaccf2
Yesudeep Mangalapilly <yesudeep@google.com>
2015-08-18 21:02:37 +0530

    carmine/server: adds a NotFound method on the server.

--- src/golang/src/carmine/default/src/server.go
+++ src/golang/src/carmine/default/src/server.go
@@ -15,12 +15,15 @@ type Server struct {
 
 // NewServer constructs a new server using the specified configuration.
 func NewServer(config Config) (*Server, error) {
-	s := &Server{
-		config: config,
-	}
-
+	s := &Server{config: config}
 	s.template.index = parse(config.TemplatesPath, "base.html", "index.html")
 	return s, nil
+}
+
+// NotFound writes as error resonse: 404.
+func (s *Server) NotFound(w http.ResponseWriter, r *http.Request) {
+	// TODO(yesudeep): Replace this with a custom template rendering later.
+	http.NotFound(w, r)
 }
 
 // ServeHTTP implements the http.Handler interface.
@@ -32,8 +35,7 @@ func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		t = s.template.index
 		w.Header().Set("Content-Type", "text/html")
 	default:
-		// This can be replaced later.
-		http.NotFound(w, r)
+		s.NotFound(w, r)
 	}
 
 	err := t.Execute(w, nil)

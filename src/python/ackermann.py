c48d9f2ea6b8f4023c224969627b5946ca255447
Yesudeep Mangalapilly <yesudeep@google.com>
2015-09-11 19:29:25 +0530

    one that displays the call stack.

--- src/python/ackermann.py
+++ src/python/ackermann.py
@@ -22,6 +22,16 @@ def ack():
       print('ackermann(%d,%d) = %d (%s)' % (m, n, ret, (end - start) * 1000.0))
 
 
+def A(m, n, s="%s"):
+  print s % ("A(%d,%d)" % (m, n))
+  if m == 0:
+    return n + 1
+  if n == 0:
+    return A(m - 1, 1, s)
+  n2 = A(m, n - 1, s % ("A(%d,%%s)" % (m - 1)))
+  return A(m - 1, n2, s)
+
+
 if __name__ == '__main__':
   sys.setrecursionlimit(1500)
   ack()

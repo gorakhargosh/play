d2278bc1b54903b9d11ab3ee4b1029f3b8aafe53
Yesudeep Mangalapilly <yesudeep@google.com>
2015-09-11 11:09:53 +0530

    ackermann in both.

--- src/python/ackermann.py
+++ src/python/ackermann.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+import time
+
 def ackermann(m, n):
   """ackermann computes ackermann computes ackermann."""
   if m == 0:
@@ -8,3 +10,16 @@ def ackermann(m, n):
     return ackermann(m-1, 1)
   else:
     return ackermann(m-1, ackermann(m, n-1))
+
+
+def ack():
+  for m in range(0, 10):
+    for n in range(0, 10):
+      start = time.time()
+      ret = ackermann(m, n)
+      end = time.time()
+      print('ackermann(%d,%d) = %d (%s)' % (m, n, ret, (end - start) * 1000.0))
+
+
+if __name__ == '__main__':
+  ack()

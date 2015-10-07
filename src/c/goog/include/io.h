58e698df6ecf2fbe53704bf1805176f11cd225d7
Yesudeep Mangalapilly <yesudeep@google.com>
2015-10-07 22:34:34 +0530

    io.

--- src/c/goog/include/io.h
+++ src/c/goog/include/io.h
@@ -4,12 +4,20 @@
 #include <stdio.h>
 
 /**
- * Reads a line from fp into s upto maxlen, EOF, or EOL.
+ * Reads a line from fp into s upto maxlen, EOF, or EOL excluding the EOL.
  *
  * @param fp     Pointer to the file from which to read.
  * @param s      The array of characters into which to read.
  * @param maxlen The maximum length of input to read.
  */
 int goog_fgetline(FILE *fp, char s[], int maxlen);
+
+/**
+ * Reads a line from stdin into s upto maxlen, EOF, or EOL excluding the EOL.
+ *
+ * @param s      The array of characters into which to read.
+ * @param maxlen The maximum length of input to read.
+ */
+int goog_getline(char s[], int maxlen);
 
 #endif /* _GOOG_IO_H_ */
--- src/c/goog/io.c
+++ src/c/goog/io.c
@@ -3,18 +3,15 @@
 
 #include "io.h"
 
-// goog_fgetline reads a line from fp into line upto maxlen excluding the
-// newline.
 // Based on ftp://ftp.eskimo.com/home/scs/cclass/week2/fgetline.c
 int goog_fgetline(FILE *fp, char s[], int maxlen) {
   int nch = 0;
-  int c;
+  int c = 0;
   maxlen = maxlen - 1;  // leave room for '\0'.
   while ((c = getc(fp)) != EOF) {
     if (c == '\n') {
       break;
     }
-
     if (nch < maxlen) {
       s[nch++] = c;
     }
@@ -24,4 +21,8 @@ int goog_fgetline(FILE *fp, char s[], int maxlen) {
   }
   s[nch] = '\0';
   return nch;
+}
+
+int goog_getline(char s[], int maxlen) {
+  return goog_fgetline(stdin, s, maxlen);
 }

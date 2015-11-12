823cfb3ff412f8486922bd4f0d318279ab662bc2
Yesudeep Mangalapilly <yesudeep@google.com>
2015-11-12 11:48:02 -0800

    Random colors.

--- src/golang/src/gopl/server2.go
+++ src/golang/src/gopl/server2.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"image"
 	"image/color"
+	"image/color/palette"
 	"image/gif"
 	"io"
 	"log"
@@ -11,17 +12,17 @@ import (
 	"math/rand"
 	"net/http"
 	"sync"
+	"time"
 )
 
 var mu sync.Mutex
 var count int
 
-var palette = []color.Color{color.White, color.Black}
-
-const (
-	whiteIndex = iota
-	blackIndex
-)
+// var palette = []color.Color{
+// 	color.White,
+// 	color.Black,
+// 	color.RGBA{0xcc, 0xff, 0xff, 0xff},
+// }
 
 const (
 	cycles  = 5
@@ -31,18 +32,24 @@ const (
 	delay   = 8
 )
 
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
 // lissajous writes a Lissajous animated GIF to the output.
 func lissajous(out io.Writer) {
+	pal := []color.Color{color.White}
+	pal = append(pal, palette.WebSafe...)
 	freq := rand.Float64() * 3.0
 	anim := gif.GIF{LoopCount: nframes}
 	phase := 0.0
 	for i := 0; i < nframes; i++ {
 		rect := image.Rect(0, 0, 2*size+1, 2*size+1)
-		img := image.NewPaletted(rect, palette)
+		img := image.NewPaletted(rect, pal)
 		for t := 0.0; t < cycles*2*math.Pi; t += res {
 			x := math.Sin(t)
 			y := math.Sin(t*freq + phase)
-			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5), blackIndex)
+			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5), uint8(rand.Intn(len(pal))))
 		}
 		phase += 0.1
 		anim.Delay = append(anim.Delay, delay)

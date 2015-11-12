1a7151d050714864527d90f8c05fb5f359b1333e
Yesudeep Mangalapilly <yesudeep@google.com>
2015-11-12 11:18:10 -0800

    Some amazing code.

---
+++ src/golang/src/gopl/dup1.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	counts := make(map[string]int)
+	input := bufio.NewScanner(os.Stdin)
+	for input.Scan() {
+		counts[input.Text()]++
+	}
+	for line, n := range counts {
+		if n > 1 {
+			// The line content is captured without the EOL character.
+			fmt.Printf("%d\t%s\n", n, line)
+		}
+	}
+}
